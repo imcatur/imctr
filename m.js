f42e195552086bb83104ec509e7dc322382800ac
imcatur <87684636+imcatur@users.noreply.github.com>
2022-03-11 20:59:00 -0800

    Update and rename j.js to m.js

--- m.js
+++ m.js
@@ -1,20 +1,6 @@
 		function el(id){
 			return document.getElementById(id);
 		}
-		var msg = [
-			{
-				"img" : "https://png.pngtree.com/png-vector/20201119/ourlarge/pngtree-gift-box-vector-isolated-in-white-png-image_2460575.jpg",
-				"txt" : "Mira Telah Memesan <br>Paket Aplikasi Lengkap, <br>Seharga 200K."
-			},
-			{
-				"img" : "https://png.pngtree.com/png-vector/20191122/ourmid/pngtree-red-gift-box-vector-illustration-with-cute-design-isolated-on-white-png-image_2016770.jpg",
-				"txt" : "Tina Telah Memesan <br>Paket Aplikasi Lengkap, <br>Seharga 200K."
-			},
-			{
-				"img" : "https://media.istockphoto.com/vectors/gift-boxes-isolated-on-white-3d-white-gift-boxes-with-golden-ribbon-vector-id1340030448?b=1&k=20&m=1340030448&s=612x612&w=0&h=Vsw4UEvd4tr52WlQAd6B6m-oeyVmt8c-k7VPdHp3wpA=",
-				"txt" : "Rani Telah Memesan <br>Paket Aplikasi Lengkap, <br>Seharga 200K."
-			}
-		];
 		var note = el("note-8594");
 		var close_note = el("close-note-4522");
 		var note_img = el("note-img-6654");
