1e968ee94f69f24894e3ff7b1deacd5182ee7cae
imcatur <87684636+imcatur@users.noreply.github.com>
2022-03-12 17:37:34 -0800

    Update and rename ss.js to x.js

---
+++ x.js
@@ -0,0 +1,15 @@
+var khlit8nbdgerjj_global_img = 'https://blogger.googleusercontent.com/img/a/AVvXsEjL5_O4oXweKUEx76kcuTjPxa4_zvtpciYocoEqCK_EP8aa0DyVhgHByebauECy13lSS7qpdRWA1LxB-c98vKhJnX4P1RJXTuntWg7Al9-tpRD_UhYhpu-0py45wkIsE1VoFRUsbCFEJujdXOx3l1GlvnhzjN5VdXcFH-y58XsdHSxIUbjnmTo3kPCG';
+var msg = [
+			{
+				"img" : khlit8nbdgerjj_global_img,
+				"txt" : "Mira Telah Memesan <br>Pembuatan Blog Dan Website, <br>Seharga 200K."
+			},
+			{
+				"img" : khlit8nbdgerjj_global_img,
+				"txt" : "Tina Telah Memesan <br>Toko Online, <br>Seharga 200K."
+			},
+			{
+				"img" : khlit8nbdgerjj_global_img,
+				"txt" : "Rani Telah Memesan <br>Pemasangan Web Server, <br>Seharga 200K."
+			}
+		];
