eb15522bc1a75a9616174a292cbba9955742a6db
imcatur <87684636+imcatur@users.noreply.github.com>
2022-03-12 17:31:31 -0800

    Update and rename s.js to ss.js

---
+++ ss.js
@@ -0,0 +1,15 @@
+var khlit8nbdgerjj_global_img = 'https://blogger.googleusercontent.com/img/a/AVvXsEj-xDI17nFQZx8L24FXoWMaOZyIjWMsr-3dOXt2MTX3y7UnWkDGqt7ixi7fcehNof1CTTt0mPS6b1YdkrYVyoDqZfugaXBS6wxH7mYvc2nPI934R6cZ3EOeNAVP9PL-T9-vQ8flbH45e9oAX4me8jdHTx9b5whgYQnhzkZCr-nj5hA8fxm9uQ0-GDYy';
+var msg = [
+			{
+				"img" : khlit8nbdgerjj_global_img,
+				"txt" : "Mira Telah Memesan <br>Paket Aplikasi Lengkap, <br>Seharga 200K."
+			},
+			{
+				"img" : khlit8nbdgerjj_global_img,
+				"txt" : "Tina Telah Memesan <br>Paket Aplikasi Lengkap, <br>Seharga 200K."
+			},
+			{
+				"img" : khlit8nbdgerjj_global_img,
+				"txt" : "Rani Telah Memesan <br>Paket Aplikasi Lengkap, <br>Seharga 200K."
+			}
+		];
